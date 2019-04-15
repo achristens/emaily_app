da4ddf22a153b8904ccd92262e94468f3ee94e20
Abby Christens <achristens@gmail.com>
2019-04-15 14:55:44 -0400

    Changed greeting

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ lou: "magical pup" });
 });
 
 const PORT = process.env.PORT || 5000;
