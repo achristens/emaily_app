3df2cec5f65c99c875cdeddcb8a3d5b87603fb3b
Abby Christens <achristens@gmail.com>
2019-06-21 11:15:11 -0400

    Updating login/logout routes and header information, passing user info to reducer, refactoring to simplify code

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = ()=> <h2>Header</h2>
-const Dashboard = ()=> <h2>Dashboard</h2>
-const SurveyNew = ()=> <h2>SurveyNew</h2>
-const Landing = ()=> <h2>Landing</h2>
+import Header from './Header';
+import * as actions from '../actions';
 
-const App = ()=> {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Route exact={true} path="/" component={Landing} />
-          <Route path="/surveys" component={Dashboard} />
-        </div>
-      </BrowserRouter>
-    </div>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
-  )
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+          return (
+            <div className="container">
+              <BrowserRouter>
+                <div>
+                  <Header />
+                  <Route exact path="/" component={Landing} />
+                  <Route exact path="/surveys" component={Dashboard} />
+                  <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+              </BrowserRouter>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default connect(null, actions)(App);
