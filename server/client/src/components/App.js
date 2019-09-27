c3eba347aee825ed8583ec3d08c208aa9b042e3d
Abby Christens <achristens@gmail.com>
2019-09-27 10:03:51 -0400

    Creating surveynew and surveyform components

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -5,9 +5,8 @@ import { connect } from 'react-redux';
 import Header from './Header';
 import Landing from './Landing';
 import * as actions from '../actions';
-
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
     componentDidMount() {
